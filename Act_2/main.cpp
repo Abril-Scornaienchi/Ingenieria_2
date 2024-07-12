0dacd77bf58c89841f29ed8a3320916f96e1fb3f
Abril-Scornaienchi <abril.scornaienchi@gmail.com>
2024-07-12 20:55:42 -0300

    CMake

---
+++ Act_2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <my_library.h>
+
+int main()
+{
+    std::cout << "Hello, world! \n";
+    std::cout << "Random number is: " << get_random_number() << "\n";
+    return 0;
+}
---
+++ Act_4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int foo()
+{
+    return 0;
+}
+
+int main() 
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
